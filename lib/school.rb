758a35b88967e472e90f626d1bf7dd203fdd683b
Ryna Truong <rynatruong@outlook.com>
2020-07-08 18:04:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(roster)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+    roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sort = {}
+    @roster.each do |grade, name|
+      sort[grade]=name.sort!
+    end
+    sort
+  end
+end
